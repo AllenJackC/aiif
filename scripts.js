909c8858a8c1074b0d4029518575306c434bfd2f
AllenJackC <51028531+AllenJackC@users.noreply.github.com>
2024-07-25 11:12:26 -0400

    First draft

--- scripts.js
+++ scripts.js
@@ -1,7 +1,11 @@
 /* Default variables */
 var isGoing = false; //Animations are not ongoing
 var awaitingButtons = false; //Not waiting for button inputs
+var awaitingInitialInput = true; //Waiting for initial input
+var initialScrawl; //Blank interval variable for initial text
 var prevSection; //There is no previous section
+var initialCounter = 0; //Count how many times interval has gone through
+var infiniteScroll;
 
 /* Extend addClass to accept callbacks */
 var oAddClass = $.fn.addClass;
@@ -25,10 +29,24 @@ function scrollToBottom() {
 function toggleInput() {
     if (!awaitingButtons) {
         $(window).on('keypress', function() {
-            nextUp($('.hidden:first'));
+            if (awaitingInitialInput) {
+                $('.optional').removeClass('hidden');
+                nextUp($('#wait-input'));
+                awaitingInitialInput = false;
+                clearInterval(initialScrawl);
+            } else {
+                nextUp($('.hidden:first'));
+            }
         });
         $(window).click(function(e) {
-            if (!$(e.target).hasClass('slider') && $(e.target).attr('id') != 'accessible' && $(e.target).attr('id') != 'accessible-label' && !$(e.target).is('a')) nextUp($('.hidden:first'));
+            if (awaitingInitialInput) {
+                $('.optional').removeClass('hidden');
+                nextUp($('#wait-input'));
+                awaitingInitialInput = false;
+                clearInterval(initialScrawl);
+            } else {
+                if (!$(e.target).hasClass('slider') && $(e.target).attr('id') != 'accessible' && $(e.target).attr('id') != 'accessible-label' && !$(e.target).is('a')) nextUp($('.hidden:first'));
+            }
         });
     } else {
         $(window).unbind('keypress');
@@ -55,42 +73,140 @@ function nextUp(curEl) {
                     prevSection = curEl; //Set the previous section as the current text
                     isGoing = false; //Animation finished
                     //If current text is part of a collection of multiple paths, disable the other options
-                    if (curEl.parent().is('.multiple-paths')) curEl.siblings().removeClass('hidden').css('display', 'none');
+                    $('[data-disable*="' + curEl.attr('data-target') + '"]').removeClass('hidden').css('display', 'none');
+                    //If the next element is a question, show it right away
+                    if (curEl.hasClass('question')) {
+                        curEl = $('.hidden:first');
+                        var buttonContainer = curEl.closest('.button-container'); //Identify the button container
+                        awaitingButtons = true; //Waiting for button selections
+                        toggleInput(); //Disable general inputs while waiting for buttons
+                        buttonContainer.removeClass('hidden').css('display', 'grid'); //Show the button container
+                        //If the page is in accessibility mode, fade the buttons in
+                        if ($('body').hasClass('accessible')) {
+                            buttonContainer.find('button').each(function(i) {
+                                var buttonAnim = $(this);
+                                setTimeout(function() {
+                                    buttonAnim.addClass('fade-in');
+                                }, 100 * i);
+                            }).promise().done(function() {
+                                isGoing = false; //Once loop through buttons is done, animations are done
+                            });
+                            //If the page is NOT in accessibility mode, flicker the buttons in
+                        } else {
+                            buttonContainer.find('button').each(function(i) {
+                                var buttonAnim = $(this);
+                                setTimeout(function() {
+                                    buttonAnim.addClass('flicker');
+                                }, 100 * i);
+                            }).promise().done(function() {
+                                isGoing = false; //Once loop through buttons is done, animations are done
+                            });
+                        }
+                        //If it's the last message, show last few links, disable inputs and enable scrolling
+                    } else if (curEl.hasClass('last')) {
+                        //If there's a previous section, remove the cursor from that text
+                        if (prevSection) prevSection.text(prevSection.text().slice(0, -1));
+                        //If the page is in accessibility mode, fade the links in
+                        if ($('body').hasClass('accessible')) {
+                            $('.ending').each(function(i) {
+                                var linkAnim = $(this);
+                                setTimeout(function() {
+                                    linkAnim.removeClass('hidden');
+                                    linkAnim.addClass('fade-in');
+                                }, 100 * i);
+                            }).promise().done(function() {
+                                clearInterval(infiniteScroll).done(function() {
+                                    $('body').css('overflow-y', 'auto'); //Once loop through links is done, enable scrolling
+                                });
+                            });
+                            //If the page is NOT in accessibility mode, flicker the links in
+                        } else {
+                            $('.ending').each(function(i) {
+                                var linkAnim = $(this);
+                                setTimeout(function() {
+                                    linkAnim.removeClass('hidden');
+                                    linkAnim.addClass('flicker');
+                                }, 100 * i);
+                            }).promise().done(function() {
+                                clearInterval(infiniteScroll).done(function() {
+                                    $('body').css('overflow-y', 'auto'); //Once loop through links is done, enable scrolling
+                                });
+                            });
+                        }
+                    } else {
+                        isGoing = false; //Animations done
+                    }
                 }
             });
-        } else {
-            var buttonContainer = curEl.closest('.button-container'); //Identify the button container
-            isGoing = true; //Animations are ongoing
-            awaitingButtons = true; //Waiting for button selections
-            toggleInput(); //Disable general inputs while waiting for buttons
-            buttonContainer.removeClass('hidden').css('display', 'grid'); //Show the button container
-            //If the page is in accessibility mode, fade the buttons in
+            //If the current element is a link
+        } else if (curEl.is('a')) {
+            //If the page is in accessibility mode, fade the link in
             if ($('body').hasClass('accessible')) {
-                buttonContainer.find('button').each(function(i) {
-                    var buttonAnim = $(this);
-                    setTimeout(function() {
-                        buttonAnim.addClass('fade-in');
-                    }, 100 * i);
-                }).promise().done(function() {
-                    isGoing = false; //Once loop through buttons is done, animations are done
-                });
-                //If the page is NOT in accessibility mode, flicker the buttons in
+                curEl.removeClass('hidden');
+                curEl.addClass('fade-in');
+                isGoing = false; //Animations are done
+                //If the page is NOT in accessibility mode, flicker the link in
             } else {
-                buttonContainer.find('button').each(function(i) {
-                    var buttonAnim = $(this);
-                    setTimeout(function() {
-                        buttonAnim.addClass('flicker');
-                    }, 100 * i);
-                }).promise().done(function() {
-                    isGoing = false; //Once loop through buttons is done, animations are done
-                });
+                curEl.removeClass('hidden');
+                curEl.addClass('flicker');
+                isGoing = false; //Animations are done
             }
-        }
+            //If the current element is a loading animation
+        } else if (curEl.is('.loader')) {
+            isGoing = true; //Animations are ongoing
+            curEl.removeClass('hidden'); //Show the loading animation
+            //After three seconds, show advance the text
+            setTimeout(function() {
+                curEl.hide();
+                curEl = $('.hidden:first');
+                curEl.removeClass('hidden'); //Remove the hidden class
+                //If there's a previous section, remove the cursor from that text
+                if (prevSection) prevSection.text(prevSection.text().slice(0, -1));
+
+                /* Typewriter effect */
+                curEl.typewrite({
+                    'delay': 30,
+                    'extra_char': '<span class="cursor">▓</span>',
+                    'callback': function() {
+                        prevSection = curEl; //Set the previous section as the current text
+                        isGoing = false; //Animation finished
+                        //If current text is part of a collection of multiple paths, disable the other options
+                        $('[data-disable*="' + curEl.attr('data-target') + '"]').removeClass('hidden').css('display', 'none');
+                    }
+                });
+            }, 5000);
+        } else {
+			var buttonContainer = curEl.closest('.button-container'); //Identify the button container
+			awaitingButtons = true; //Waiting for button selections
+			toggleInput(); //Disable general inputs while waiting for buttons
+			buttonContainer.removeClass('hidden').css('display', 'grid'); //Show the button container
+			//If the page is in accessibility mode, fade the buttons in
+			if ($('body').hasClass('accessible')) {
+				buttonContainer.find('button').each(function(i) {
+					var buttonAnim = $(this);
+					setTimeout(function() {
+						buttonAnim.addClass('fade-in');
+					}, 100 * i);
+				}).promise().done(function() {
+					isGoing = false; //Once loop through buttons is done, animations are done
+				});
+				//If the page is NOT in accessibility mode, flicker the buttons in
+			} else {
+				buttonContainer.find('button').each(function(i) {
+					var buttonAnim = $(this);
+					setTimeout(function() {
+						buttonAnim.addClass('flicker');
+					}, 100 * i);
+				}).promise().done(function() {
+					isGoing = false; //Once loop through buttons is done, animations are done
+				});
+			}
+		}
     }
 }
 
 $(document).ready(function() {
-    setInterval(scrollToBottom, 10); //Scroll to bottom of the page every 10 milliseconds
+    infiniteScroll = setInterval(scrollToBottom, 10); //Scroll to bottom of the page every 10 milliseconds
 
     /* Toggle accessibility mode on load and on click */
     if ($('#accessible').is(':checked')) {
@@ -101,8 +217,6 @@ $(document).ready(function() {
         $('body').toggleClass('crt');
         $('body').toggleClass('accessible');
     });
-
-    toggleInput(); //If not waiting for button selections, any keypress, click or tap will advance the text
 
     /* If a button is clicked, change it's style, advance relevant text and disable unused text */
     $('button').click(function() {
@@ -121,8 +235,8 @@ $(document).ready(function() {
 
     /* If a single selection button is clicked, show associated text */
     $('.single button').click(function() {
-        var textID = $(this).attr('for-text'); //Get the stored association in the button
-        nextUp($('#' + textID)); //Target the associated text
+        var textID = $(this).attr('id'); //Get the stored association in the button
+        nextUp($('[data-target="' + textID + '"]')); //Target the associated text
         $(this).closest('.button-container').find('button').addClass('disabled'); //Disable interactions with all buttons on the same level
         awaitingButtons = false; //No longer waiting for button inputs
         toggleInput(); //Re-enable window inputs
@@ -135,4 +249,26 @@ $(document).ready(function() {
         awaitingButtons = false; //No longer waiting for button inputs
         toggleInput(); //Re-enable window inputs
     });
+
+    /* Special button behaviour for trick question */
+    $('#trick-question button').click(function() {
+        $('#trick-answer').text('Wait! I didn’t say that, I said "' + $(this).text() + '"');
+		if ($('body').hasClass('accessible')) {
+			$(this).addClass('slow-fade-in').text('You’re right, that is woke garbage.');
+		} else {
+			$(this).addClass('slow-flicker').text('You’re right, that is woke garbage.');
+		}
+    });
+
+    /* Initial run */
+    setTimeout(function() {
+        nextUp($('.hidden:first'));
+        toggleInput();
+        initialScrawl = setInterval(function() {
+            if (initialCounter < 5) {
+                initialCounter++;
+                nextUp($('.hidden:first'));
+            }
+        }, 5000);
+    }, 3000);
 });
